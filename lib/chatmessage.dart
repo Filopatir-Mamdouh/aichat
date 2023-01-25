380e283d5876e24b93fe63b9631e517380290134
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-01-25 18:45:19 +0200

    made a circle avatar containg the first later of the sender in the message

--- lib/chatmessage.dart
+++ lib/chatmessage.dart
@@ -7,6 +7,19 @@ class ChatMessage extends StatelessWidget {
   const ChatMessage({super.key, required this.text, required this.sender});
 
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              child: Text(sender[0]),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
