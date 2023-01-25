5c7fe71c10b186e36e41eca82b1ee8f6902a1ba7
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-01-25 18:19:46 +0200

    made the Background Matrial app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ai Chat',
+      theme: ThemeData(
+        primaryColor: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
 }
