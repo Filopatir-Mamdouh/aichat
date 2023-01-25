a6546213c52b52695dd00313b5ea703cb38b5e42
Filopatir-Mamdouh <Filopatirmamdouh@gmail.com>
2023-01-25 18:27:01 +0200

    made class for front end of the chat and desiging it

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aichat/screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.blueGrey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      home: const ChatbotScreen(),
     );
   }
 }
---
+++ lib/screen.dart
@@ -0,0 +1,16 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class ChatbotScreen extends StatefulWidget {
+  const ChatbotScreen({super.key})
+
+  @override
+  _ChatbotScreenState createState() => _ChatbotScreenState();
+}
+
+class _ChatbotScreenState extends State<ChatbotScreen> {
+
+}
+
+
